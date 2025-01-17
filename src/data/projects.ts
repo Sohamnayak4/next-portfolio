012a4d021468957f238e1a77a186f4bc12ca4a49
Soham Nayak <sohamnayak04@gmail.com>
2025-01-17 15:34:50 +0530

    minor change

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,7 +5,7 @@ const projects: IProjectItem[] = [
     id: "Chat App MERN",
     title: "Chat App",
     description:
-      "A responsive and interactive chat application developed using the MERN stack (MongoDB, Express.js, React.js, Node.js) and Socket.IO. The app enables real-time messaging, features a sleek user interface built with React, and uses MongoDB for efficient data storage. It ensures seamless performance across devices.",
+      "A responsive and interactive chat application developed using the MERN stack (MongoDB, Express.js, React.js, Node.js) and Socket.IO. The app enables real-time messaging, features a sleek user interface built with React, and uses MongoDB for efficient data storage.",
     icon: "/skills/react.svg",
     repoType: RepoType.Public,
     projectType: ProjectType.Personal,
@@ -24,7 +24,7 @@ const projects: IProjectItem[] = [
     id: "Task Manager MERN",
     title: "Task Manager",
     description:
-      "A feature-rich task manager application built using the MERN stack (MongoDB, Express.js, React.js, Node.js). The app allows users to create, update, and manage tasks efficiently, with a dynamic React-based frontend and a robust backend powered by Express and Node.js. MongoDB ensures scalable and reliable data storage, providing a seamless user experience across devices.",
+      "A feature-rich task manager application built using the MERN stack (MongoDB, Express.js, React.js, Node.js). The app allows users to create, update, and manage tasks efficiently, with a dynamic React-based frontend and a robust backend powered by Express and Node.js.",
     icon: "/skills/react.svg",
     repoType: RepoType.Public,
     projectType: ProjectType.Personal,
@@ -36,7 +36,7 @@ const projects: IProjectItem[] = [
     id: "FocusTube Landing Page",
     title: "FocusTube",
     description:
-      "A modern and visually appealing landing page designed for FocusTube, a SaaS productivity tool tailored for YouTube users. The page highlights key features, benefits, and subscription plans, with a clean and responsive design that ensures an engaging user experience across all devices. Built to attract and convert visitors effectively.",
+      "A modern and visually appealing landing page designed for FocusTube, a SaaS productivity tool tailored for YouTube users. The page highlights key features, benefits, and subscription plans, with a clean and responsive design that ensures an engaging user experience across all devices.",
     icon: "/skills/react.svg",
     repoType: RepoType.Private,
     projectType: ProjectType.Personal,
